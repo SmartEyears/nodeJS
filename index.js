37c23d3b2b7aade59fa6392da0c71e73be52d68a
SmartEyears <57298689+SmartEyears@users.noreply.github.com>
2020-06-17 22:16:21 +0900

    middleware

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hellow World!");
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from Home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("미들웨어!");
+  next();
+};
+
+app.use(betweenHome);
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
