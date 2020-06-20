9a7057d7cdb08ea5a8744c9e785d7d843be5007e
SmartEyears <57298689+SmartEyears@users.noreply.github.com>
2020-06-20 23:39:49 +0900

    view_Pug

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,16 +3,16 @@ import routes from "../routes";
 import {
   videos,
   upload,
-  videosDetail,
   editVideo,
   deleteVideo,
+  videoDetail,
 } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videosDetail, videosDetail);
+videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ const CHANGE_PASSWORD = "/change-password";
 // Videos
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
-const VIDEOS_DETAIL = "/:id";
+const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
@@ -30,7 +30,7 @@ const routes = {
   changePassword: CHANGE_PASSWORD,
   videos: VIDEOS,
   upload: UPLOAD,
-  videosDetail: VIDEOS_DETAIL,
+  videoDetail: VIDEO_DETAIL,
   editVideo: EDIT_VIDEO,
   deleteVideo: DELETE_VIDEO,
 };
